69c84efcfee60f171bd2cfe2a52b870cd9042f5e
Mourougen MRMT <97475510+MourouMRMT@users.noreply.github.com>
2024-01-18 10:08:46 +0100

    ajout bcd7seg et calibration

--- software/app/main_timer.c
+++ software/app/main_timer.c
@@ -11,9 +11,11 @@
 #include"opencores_i2c_regs.h"
 #include "altera_avalon_timer_regs.h"
 #include "altera_avalon_timer.h"
+#include "altera_avalon_pio_regs.h"
 
 uint8_t X0,X1,Y0,Y1,Z0,Z1;
-	
+int16_t X,Y,Z;
+uint8_t flag=0;
 #define ADXL345_addr 0x1D
 #define I2C_SPEED 100000
 #define DATAX0 0x32
@@ -52,12 +54,56 @@ void calibrate_adxl345()
 {
 	write_adxl345(reg_OFSX,0);
 	write_adxl345(reg_OFSY,0);
-	write_adxl345(reg_OFSZ,0);
+	write_adxl345(reg_OFSZ,-29);
 
 }
 
+int16_t complement(int16_t value)
+{
+	if(value & 0x8000)
+	{
+		value=-(0xFFFF - value +1);
+	}
+	
+	return value*3.9;
+}
+
+void print_to_7seg(int16_t value)
+{
+	int8_t data[5] = {0,0,0,0,0};
+	if (value < 0)
+	{
+		data[0] = value % 10 ;
+		data[1] = (value/10) % 10 ;
+		data[2] = (value/100) % 10 ;
+		data[3] = value / 1000 ;
+		data[4] = 10;			
+	}
+	else
+	{
+		data[0] = value % 10 ;
+		data[1] = (value/10) % 10 ;
+		data[2] = (value/100) % 10 ;
+		data[3] = value / 1000 ;
+		data[4] = 11;		
+	}
+
+	
+	
+	// Write on BCD 7 SEGMENTS
+	IOWR_ALTERA_AVALON_PIO_DATA(SEG1_BASE,data[0]);
+	IOWR_ALTERA_AVALON_PIO_DATA(SEG2_BASE,data[1]);
+	IOWR_ALTERA_AVALON_PIO_DATA(SEG3_BASE,data[2]);
+	IOWR_ALTERA_AVALON_PIO_DATA(SEG4_BASE,data[3]);
+	IOWR_ALTERA_AVALON_PIO_DATA(SEG5_BASE,data[4]);
+	
+	
+}
+
 static void timer_irq(void *Context)
-{	
+{
+
+	
 	X0=read_adxl345(DATAX0);
 	X1=read_adxl345(DATAX1);
 	Y0=read_adxl345(DATAY0);
@@ -65,25 +111,65 @@ static void timer_irq(void *Context)
 	Z0=read_adxl345(DATAZ0);
 	Z1=read_adxl345(DATAZ1);
 	
-	alt_printf("X: %x%x Y: %x%x Z: %x%x\n",X0,X1,Y0,Y1,Z0,Z1);
+	
+	//complement a deux
+	X= (X1 << 8) | X0;
+	Y= (Y1 << 8) | Y0;
+	Z= (Z1 << 8) | Z0;
+	
+	complement(X);
+	complement(Y);
+	complement(Z);
+	
+	alt_printf("X: %x Y: %x Z: %x\n",X,Y,Z);
 	
 	IOWR_ALTERA_AVALON_TIMER_STATUS(TIMER_0_BASE, 0b1);
 }
 
 
+static void key_irq(void *Context)
+{
+	alt_printf("KEY Interrupt\n");
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BTN_BASE,0b1);
+	
+	flag++;
+	if (flag>3){flag=0;}
+	
+
+}
 
 int main()
 {
-	
+	// Enable IRQ end button capture
+	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(BTN_BASE,0b1);
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BTN_BASE,0b1);
 
 	I2C_init(OPENCORES_I2C_0_BASE,ALT_CPU_CPU_FREQ,I2C_SPEED);
 	
 	calibrate_adxl345();
+	//write_adxl345(0x31, 0x7);
 	
 	// Register IRQ
 	alt_irq_register(TIMER_0_IRQ,NULL,timer_irq);
+	alt_irq_register(BTN_IRQ,NULL,key_irq);
 	
-	while(1){}
+	while(1)
+	{
+		switch(flag)
+		{
+			case 0 : print_to_7seg(X);
+			break;
+			
+			case 1 : print_to_7seg(Y);
+			break;
+			
+			case 2 : print_to_7seg(Z);
+			break;
+			
+			default	: print_to_7seg(X);	
+		}
+		
+	}
 	
 	return 0;
 	
--- toplevel.vhd
+++ toplevel.vhd
@@ -4,10 +4,17 @@ use IEEE.numeric_std.all;
 
 entity toplevel is
 	port (
-		CLK                             : in    std_logic := '0'; -- clk.clk
-		SCL : inout std_logic := '0'; -- .scl_pad_io
-		SDA : inout std_logic := '0'; --                         .sda_pad_io
-		RESET                       : in    std_logic := '0'  -- reset.reset_n
+
+		CLK                             : in    std_logic                    := '0'; --                      clk.clk
+		SCL : inout std_logic                    := '0'; -- opencores_i2c_0_export_0.scl_pad_io
+		SDA : inout std_logic                    := '0'; --  .sda_pad_io
+		RESET                       : in    std_logic                    := '0'; --      reset.reset_n
+		BTN                          : in    std_logic                    := '0'; -- btn.export
+		SEG1     : out   std_logic_vector(7 downto 0);        -- seg1_external_connection.export
+		SEG2     : out   std_logic_vector(7 downto 0);        -- seg2_external_connection.export
+		SEG3     : out   std_logic_vector(7 downto 0);        -- seg3_external_connection.export
+		SEG4     : out   std_logic_vector(7 downto 0);        -- seg4_external_connection.export
+		SEG5     : out   std_logic_vector(7 downto 0)         -- seg5_external_connection.export
 	);
 end entity toplevel;
 
@@ -17,10 +24,29 @@ architecture rtl of toplevel is
 			clk_clk                             : in    std_logic := 'X'; -- clk
 			reset_reset_n                       : in    std_logic := 'X'; -- reset_n
 			opencores_i2c_0_export_0_scl_pad_io : inout std_logic := 'X'; -- scl_pad_io
-			opencores_i2c_0_export_0_sda_pad_io : inout std_logic := 'X'  -- sda_pad_io
+			opencores_i2c_0_export_0_sda_pad_io : inout std_logic := 'X';  -- sda_pad_io
+			btn_export                          : in    std_logic := 'X'; -- export
+			seg1_external_connection_export     : out   std_logic_vector(3 downto 0);        -- export
+			seg2_external_connection_export     : out   std_logic_vector(3 downto 0);        -- export
+			seg4_external_connection_export     : out   std_logic_vector(3 downto 0);        -- export
+			seg5_external_connection_export     : out   std_logic_vector(3 downto 0);        -- export
+			seg3_external_connection_export     : out   std_logic_vector(3 downto 0)         -- export
 		);
 	end component lab3;
-
+	
+	component BCD7SEG is
+		port (
+			input : in std_logic_vector(3 downto 0) := (others => 'X'); 
+			output : out std_logic_vector(7 downto 0)
+		);
+	end component BCD7SEG;
+	
+signal segment1 : std_logic_vector(3 downto 0);
+signal segment2 : std_logic_vector(3 downto 0);
+signal segment3 : std_logic_vector(3 downto 0);
+signal segment4 : std_logic_vector(3 downto 0);
+signal segment5 : std_logic_vector(3 downto 0);
+	
 begin
  
 	u0 : component lab3
@@ -28,7 +54,43 @@ begin
 			clk_clk                             => CLK,      --clk.clk
 			reset_reset_n                       => RESET,    -- reset.reset_n
 			opencores_i2c_0_export_0_scl_pad_io => SCL, -- .scl_pad_io
-			opencores_i2c_0_export_0_sda_pad_io => SDA  -- .sda_pad_io
+			opencores_i2c_0_export_0_sda_pad_io => SDA,  -- .sda_pad_io
+			btn_export                          => BTN,      -- btn.export
+			seg1_external_connection_export     => segment1,     -- seg1_external_connection.export
+			seg2_external_connection_export     => segment2,     -- seg2_external_connection.export
+			seg4_external_connection_export     => segment4,     -- seg4_external_connection.export
+			seg5_external_connection_export     => segment5,     -- seg5_external_connection.export
+			seg3_external_connection_export     => segment3      -- seg3_external_connection.export
+		);
+		
+	u1 : component BCD7SEG
+		port map (
+			input => segment1,
+			output => SEG1
 		);
 
+	u2 : component BCD7SEG
+		port map (
+			input => segment2,
+			output => SEG2
+		);
+		
+	u3 : component BCD7SEG
+		port map (
+			input => segment3,
+			output => SEG3
+		);		
+		
+	u4 : component BCD7SEG
+		port map (
+			input => segment4,
+			output => SEG4
+		);
+		
+	u5 : component BCD7SEG
+		port map (
+			input => segment5,
+			output => SEG5
+		);	
+		
 end architecture;
